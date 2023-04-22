46003cfd964f759fa91e52d69f1bc2925990c280
criss <criss@DESKTOP-CS6FKMA>
2023-04-22 03:09:41 -0500

    Agregar archivos de proyecto.

---
+++ CsandovalS31/Login.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CsandovalS31
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            
+        }
+        
+        //}
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contraseña = txtContraseña.Text;
+            if (usuario == "cris" && contraseña == "1988")
+            {
+                Navigation.PushAsync(new Principal());
+            }
+            else
+            {
+                Navigation.PushAsync(new Registrar());
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrecto", "Cerrar");
+            }
+        }
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registrar());
+        }
+        private void btnLimpiar_Clicked(object sender, EventArgs e)
+        {
+            txtUsuario.Text=" ";
+            txtContraseña.Text =" ";
+        }
+
+        //    private void txtdatos_TextChanged(object sender, TextChangedEventArgs  e)
+        //    {
+        //        try
+        //        {
+        //            double numero = Convert.ToDouble(txtdatos.Text); 
+        //            if(numero < 1 && numero > 10)
+        //            {
+        //                DisplayAlert("MENSAJE", "El rango permitido es 1-10", "Cerrar");
+        //                txtdatos.Text = "";
+
+        //            }
+        //        }
+        //        catch(Exception ex)
+        //        {
+        //            DisplayAlert("ERROR", ex.Message, "Cerrar");
+
+        //        }
+        //}
+        //}
+    }
+}
---
+++ CsandovalS31/Principal.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CsandovalS31
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Principal : ContentPage
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //colocar una variable double con la conversion de string a double
+
+                double DatoUno = Convert.ToDouble(txtdatoUno.Text) * 0.3;
+                double DatoDos = Convert.ToDouble(txtdatodos.Text) * 0.2;
+                double suma = DatoUno + DatoDos;
+                TxtResultado.Text = suma.ToString();
+
+                double Datotres = Convert.ToDouble(txtdatoUno.Text) * 0.3;
+                double Datocuatro = Convert.ToDouble(txtdatodos.Text) * 0.2;
+                double suma1 = DatoUno + DatoDos;
+
+                txtResultado2.Text = suma.ToString();
+
+                double NOtaFinal = suma + suma1;
+                double N0taFinal = Convert.ToDouble(txtfinal.Text);
+
+                if (N0taFinal >= 7)
+                {
+                    DisplayAlert("NOTAS ESTADO", "Tu calificacion" + N0taFinal + "\nAprobaste con" + NOtaFinal, "CERRAR");
+                }
+                else
+                 if (NOtaFinal >= 5 && NOtaFinal <= 6.9)
+                {
+                    DisplayAlert("NOTAS ESTADO", "Tu calificacion" + N0taFinal + "\nComplementario con" + NOtaFinal, "CERRAR");
+                }
+                else
+                if (NOtaFinal >= 5 && NOtaFinal < 5)
+                {
+                    DisplayAlert("NOTAS ESTADO", "Tu calificacion " + N0taFinal + "\nReprobaste con" + NOtaFinal, "CERRAR");
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias por su atencion");
+            }
+
+
+        }
+
+        private void btnLimpiar_Clicked(object sender, EventArgs e)
+        {
+            //limpiar todos los datos de la pantalla
+
+            txtName.Text = "";
+            txtdatoUno.Text = "";
+            txtdatodos.Text = "";
+            txtDatotres.Text = "";
+            txtDatocuatro.Text = "";
+            TxtResultado.Text = "";
+            txtResultado2.Text = "";
+
+        }
+    }
+}
